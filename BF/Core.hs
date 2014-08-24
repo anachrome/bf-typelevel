7466aa1ba91157cc36ad72069c7791f96f3c0a7d
Lily Anachrome <lily.anachrome@gmail.com>
2014-08-24 13:26:53 -0400

    clean up old comments and imports

--- BF/Core.hs
+++ BF/Core.hs
@@ -1,4 +1,4 @@
--- core evaluator
+-- type level bf-ast and eval function
 
 {-# LANGUAGE MultiParamTypeClasses
            , TypeOperators
@@ -41,9 +41,6 @@ data Loop loop
 --type BF = [BFElem]
 --data BFElem = Forward | Backward | IncBF | DecBF | Out | In | Loop BF
 
---class Eval bf array inp out array' inp' out'
---         | bf array inp out -> array' inp' out'
-
 type family Eval bf (aio :: (Array k, [k], [k])) :: (Array k, [k], [k]) where
     Eval EOBF '(a, i, o) = '(a, i, o)
     Eval (Forward :> next) '(a, i, o) = Eval next '(Forth a, i, o)
@@ -51,68 +48,26 @@ type family Eval bf (aio :: (Array k, [k], [k])) :: (Array k, [k], [k]) where
     Eval (IncBF :> next) '(a, i, o) = Eval next '(Inc a, i, o)
     Eval (DecBF :> next) '(a, i, o) = Eval next '(Dec a, i, o)
 
---instance Eval EOBF a i o a i o
---
---instance ( Forth a a'
---         , Eval next a' i o a'' i' o'
---         ) => Eval (Forward :> next) a i o a'' i' o'
---instance ( Back a a'
---         , Eval next a' i o a'' i' o'
---         ) => Eval (Backward :> next) a i o a'' i' o'
---instance ( Inc a a'
---         , Eval next a' i o a'' i' o'
---         ) => Eval (IncBF :> next) a i o a'' i' o'
---instance ( Dec a a'
---         , Eval next a' i o a'' i' o'
---         ) => Eval (DecBF :> next) a i o a'' i' o'
-
--- Requesting input from an empty list results in a "runtime" error.  uncomment
--- the below to prevent this (and possibly -- allow infinite loops or context
+-- Requesting input from an empty list results in a type error.  uncomment
+-- the below to prevent this (and possibly allow infinite loops or context
 -- stack overflows)
--- instance Eval (In :> next) a Nil o a Nil o
---instance ( PutCur x a ax
---         , Eval next ax inp o a' i' o'
---         ) => Eval (In :> next) a (x :* inp) o a' i' o'
+--    Eval (In :> next) '(a, '[], o) = '(a, '[], o)
 
     Eval (In :> next) '(a, (x ': inp), o) = Eval next '(PutCur x a, inp, o)
 
---instance ( GetCur a x
---         , Eval next a i (x :* o) a' i' o'
---         ) => Eval (Out :> next) a i o a' i' o'
-
     Eval (Out :> next) '(a, inp, o) = Eval next '(a, inp, (GetCur a ': o))
-
---instance ( Eval next (Array ls Zero rs) i o a' i' o'
---         ) => Eval (Loop loop :> next) (Array ls Zero rs) i o a' i' o'
---instance ( Eval loop a i o a' i' o'
---         , Eval (Loop loop :> next) a' i' o' a'' i'' o''
---         ) => Eval (Loop loop :> next) a i o a'' i'' o''
 
     Eval (Loop loop :> next) '( 'Array ls Zero rs, i, o) = Eval next '( 'Array ls Zero rs, i, o)
     Eval (Loop loop :> next) '(a, i, o) = Eval (Loop loop :> next) (Eval loop '(a, i, o))
 
 -- these three functions roughly parallel their state monad nomenbrethren
---run  :: ( Eval bf array inp out array' inp' out'
---        , Reverse out' out''
---        ) => bf -> (array,inp,out) -> (array',inp',out'')
---run  = undefined
-
 run :: '(array', inp', out') ~ Eval bf '(array, inp, out) 
     => Proxy bf -> Proxy '(array, inp, out) -> Proxy '(array', inp', Reverse out')
 run = undefined
 
---eval :: ( Eval bf array inp out array' inp' out'
---        , Reverse out' out''
---        ) => bf -> (array,inp,out) -> out''
---eval = undefined
-
 eval :: '(array', inp', out') ~ Eval bf '(array, inp, out)
      => Proxy bf -> Proxy '(array, inp, out) -> Proxy (Reverse out')
 eval = undefined
-
---exec :: ( Eval bf array inp out array' inp' out'
---        ) => bf -> (array,inp,out) -> array
---exec = undefined
 
 exec :: '(array', inp', out') ~ Eval bf '(array, inp, out)
      => Proxy bf -> Proxy '(array, inp, out) -> Proxy array'
--- BF/Types.hs
+++ BF/Types.hs
@@ -1,4 +1,4 @@
--- types used in the evaluator
+-- basic type-level structures and functions used in the evaluator
 
 {-# LANGUAGE TypeOperators
            , TypeFamilies
@@ -12,103 +12,44 @@
 
 module BF.Types where
 
-import Data.Typeable
-import Data.Typeable.Internal
-
---
--- numbers
---
-
--- type level integers
--- because for some reason Int isn't autopromoted with -XDataKinds
+-- integers, because for some reason Int isn't autopromoted with -XDataKinds
 data Int' = Zero | Succ Int' | Pred Int'
-
---data Zero
---data Succ a
---data Pred a
-
---class Incr a b | a -> b
---instance Incr Zero (Succ Zero)
---instance Incr (Succ a) (Succ (Succ a))
---instance Incr (Pred a) a
 
 type family Incr (n :: Int') where
     Incr Zero = Succ Zero
     Incr (Succ n) = Succ (Succ n)
     Incr (Pred n) = n
 
---class Decr a b | a -> b
---instance Decr Zero (Pred Zero)
---instance Decr (Succ a) a
---instance Decr (Pred a) (Pred (Pred a))
-
 type family Decr (n :: Int') where
     Decr Zero = Pred Zero
     Decr (Succ n) = n
     Decr (Pred n) = Pred (Pred n)
 
---
 -- list
---
-
---data Nil
---infixr :*
---data a :* b
-
---class Reverse' a b c | a b -> c
---instance Reverse' acc Nil acc
---instance Reverse' (x :* acc) xs xs' => Reverse' acc (x :* xs) xs'
-
 type family Reverse' (acc :: [k]) (ls :: [k]) :: [k] where
     Reverse' acc '[] = acc
     Reverse' acc (x ': xs) = Reverse' (x ': acc) xs
 
---class Reverse a b | a -> b
---instance Reverse' Nil xs xs' => Reverse xs xs'
-
 type family Reverse (ls :: [k]) :: [k] where
     Reverse ls = Reverse' '[] ls
 
---
 -- array (basically ziplist)
---
-
 data Array a = Array [a] a [a]
-
---data Array ls x rs
-
---class GetCur a x | a -> x
---instance GetCur (Array ls x rs) x
 
 type family GetCur (a :: Array t) :: t where
     GetCur ('Array ls x rs) = x
 
---class PutCur x b c | x b -> c
---instance PutCur x (Array ls y rs) (Array ls x rs)
-
 type family PutCur (x :: t) (a :: Array t) :: Array t where
     PutCur x ('Array ls y rs) = 'Array ls x rs
-
---class Inc a b | a -> b
---instance Incr x x' => Inc (Array ls x rs) (Array ls x' rs)
 
 type family Inc (a :: Array t) :: Array t where
     Inc ('Array ls x rs) = 'Array ls (Incr x) rs
 
---class Dec a b | a -> b
---instance Decr x x' => Dec (Array ls x rs) (Array ls x' rs)
-
 type family Dec (a :: Array t) :: Array t where
     Dec ('Array ls x rs) = 'Array ls (Decr x) rs
 
---class Forth a b | a -> b
---instance Forth (Array ls x (r :* rs)) (Array (x :* ls) r rs)
-
 type family Forth (a :: Array t) :: Array t where
     Forth ('Array ls x (r ': rs)) = 'Array (x ': ls) r rs
-
---class Back a b | a -> b
---instance Back (Array (l :* ls) x rs) (Array ls l (x :* rs))
 
 type family Back (a :: Array t) :: Array t where
     Back ('Array (l ': ls) x rs) = 'Array ls l (x ': rs)
